4f54d8548079cbf67ff860ff5232533bf2f08dd6
Alex Nielsen <alex.nielsen@gmail.com>
2023-04-25 17:28:13 +0000

    added loading message for sleeping backend

--- frontend/src/pages/ListAll.jsx
+++ frontend/src/pages/ListAll.jsx
@@ -1,23 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useProductsContext } from '../hooks/useProductsContext'
 import ListAllProductDetails from '../components/ListAllProductDetails'
 
-
-
 function ListAll() {
   const { products, dispatch } = useProductsContext()
-  
+
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch(
-        process.env.REACT_APP_BACKEND_URL
-      )
+      const response = await fetch(process.env.REACT_APP_BACKEND_URL)
 
       const json = await response.json()
 
       if (response.ok) {
         dispatch({ type: 'SET_PRODUCTS', payload: json })
       }
+      setLoading(false)
     }
 
     fetchProducts()
@@ -25,12 +24,25 @@ function ListAll() {
 
   return (
     <div className='list-all'>
-      <div className='products'>
-        {products &&
-          products.map((product) => (
-            <ListAllProductDetails key={product._id} product={product} />
-          ))}
-      </div>
+      {loading ? (
+        <>
+          <h3>
+            The database for this app runs on a free server that goes to sleep
+            after periods of inactivity
+            <br />
+            If you're seeing this message, please wait a minute to let the
+            server start up and then hit reload
+          </h3>
+        </> 
+      ) : (
+        <div className='products'>
+          {products &&
+            products.map((product) => (
+              <ListAllProductDetails key={product._id} product={product} />
+            ))}
+        </div>
+      )}
+     
     </div>
   )
 }

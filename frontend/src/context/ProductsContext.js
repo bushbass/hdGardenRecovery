e1f6eb81e112bdcc20e6268fe64cc2defa5a1cac
Alex Nielsen <alex@bigal.org>
2023-05-04 08:54:54 -0400

    removed unused increment case from ruducer

--- frontend/src/context/ProductsContext.js
+++ frontend/src/context/ProductsContext.js
@@ -1,22 +1,23 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer } from 'react';
 
-export const ProductsContext = createContext()
+export const ProductsContext = createContext();
+
 export const productsReducer = (state, action) => {
   switch (action.type) {
     case 'SET_PRODUCTS':
       return {
         products: action.payload,
-      }
+      };
     case 'CREATE_PRODUCT':
       return {
         products: [action.payload, ...state.products],
-      }
+      };
     case 'DELETE_PRODUCT':
       return {
         products: state.products.filter(
           (product) => product._id !== action.payload._id
         ),
-      }
+      };
     case 'UPDATE_PRODUCT':
       return {
         products: state.products.splice(
@@ -26,23 +27,21 @@ export const productsReducer = (state, action) => {
           1,
           action.payload
         ),
-      }
-    case 'INCREMENT':
-      return state
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const ProductsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(productsReducer, {
     products: null,
-  })
+  });
 
   return (
     <ProductsContext.Provider value={{ ...state, dispatch }}>
       {children}
     </ProductsContext.Provider>
-  )
-}
+  );
+};

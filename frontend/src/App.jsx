1efab6c1fa9092a80e1e0ef6e738c55587ff5af2
Alex Nielsen <alex@bigal.org>
2023-06-02 09:49:20 -0400

    added empty #top div

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AddEditProducts from './pages/AddEditProducts'
-import Edit from './pages/Edit'
-import ListAll from './pages/ListAll'
-import Navbar from './components/Navbar'
-import AddProducts from './pages/AddProducts'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddEditProducts from './pages/AddEditProducts';
+import Edit from './pages/Edit';
+import ListAll from './pages/ListAll';
+import Navbar from './components/Navbar';
+import AddProducts from './pages/AddProducts';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
+      <div id="top"></div>
       <BrowserRouter>
         <Navbar />
-        <div className='pages'>
+        <div className="pages">
           <Routes>
-            <Route path='/' element={<ListAll />}></Route>
-            <Route path='/list' element={<AddEditProducts />}></Route>
-            <Route path='/edit' element={<Edit />}></Route>
-            <Route path='/add' element={<AddProducts />}></Route>
+            <Route path="/" element={<ListAll />}></Route>
+            <Route path="/list" element={<AddEditProducts />}></Route>
+            <Route path="/edit" element={<Edit />}></Route>
+            <Route path="/add" element={<AddProducts />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

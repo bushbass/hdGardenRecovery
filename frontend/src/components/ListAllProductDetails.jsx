a3c7704953d9982b40d4ea272086ce1cf73cef6f
bushbass <alex.nielsen@gmail.com>
2023-07-15 13:49:59 -0400

    fixed product button can create negative numbers

--- frontend/src/components/ListAllProductDetails.jsx
+++ frontend/src/components/ListAllProductDetails.jsx
@@ -1,85 +1,88 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useProductsContext } from '../hooks/useProductsContext';
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useProductsContext } from '../hooks/useProductsContext'
 import { Link } from 'react-router-dom'
 
 function ListAllProductDetails({ product }) {
-  const { dispatch, products } = useProductsContext();
+  const { dispatch, products } = useProductsContext()
 
-  const id = product._id;
+  const id = product._id
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   // const [quantity, setQuantity] = useState(product.quantity);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null)
 
   const increment = async () => {
     const newProduct = {
       title: product.title,
       category: product.category,
       quantity: products.find((product) => product._id === id).quantity + 1,
-    };
+    }
     const response = await fetch(process.env.REACT_APP_BACKEND_URL + id, {
       method: 'put',
       body: JSON.stringify(newProduct),
       headers: {
         'Content-Type': 'application/json',
       },
-    });
-    const json = await response.json();
+    })
+    const json = await response.json()
 
     if (!response.ok) {
-      setError(json.error);
-      console.log(error);
+      setError(json.error)
+      console.log(error)
     }
     if (response.ok) {
-      setError(null);
+      setError(null)
       // setQuantity(json.quantity);
       // update context with new quantity
-      console.log('payload from listAllProductDetails', json);
-      dispatch({ type: 'UPDATE_PRODUCT', payload: json });
-      navigate(`/`);
+      console.log('payload from listAllProductDetails', json)
+      dispatch({ type: 'UPDATE_PRODUCT', payload: json })
+      navigate(`/`)
     }
-  };
+  }
   const decrement = async () => {
+    const currentProduct = products.find((product) => product._id === id)
+    if (currentProduct.quantity < 1) {
+      return
+    }
     const newProduct = {
       title: product.title,
       category: product.category,
-      quantity: products.find((product) => product._id === id).quantity - 1,
-    };
+      quantity: currentProduct.quantity - 1,
+    }
     const response = await fetch(process.env.REACT_APP_BACKEND_URL + id, {
       method: 'put',
       body: JSON.stringify(newProduct),
       headers: {
         'Content-Type': 'application/json',
       },
-    });
-    const json = await response.json();
+    })
+    const json = await response.json()
 
     if (!response.ok) {
-      setError(json.error);
+      setError(json.error)
     }
     if (response.ok) {
-      setError(null);
-      console.log('payload from listAllProductDetails', json);
-      dispatch({ type: 'UPDATE_PRODUCT', payload: json });
-      navigate(`/`);
+      setError(null)
+      console.log('payload from listAllProductDetails', json)
+      dispatch({ type: 'UPDATE_PRODUCT', payload: json })
+      navigate(`/`)
     }
-  };
+  }
 
   return (
-    <div className="product-details">
+    <div className='product-details'>
       <Link to='/edit/' state={{ id: product._id }}>
-          
-      <p className="product-details-title">{product.title}</p>
-        </Link>
-      <p className="product-details-properties">
+        <p className='product-details-title'>{product.title}</p>
+      </Link>
+      <p className='product-details-properties'>
         {products.find((product) => product._id === id).quantity}{' '}
         <button onClick={increment}>+</button>{' '}
         <button onClick={decrement}>-</button>
       </p>
     </div>
-  );
+  )
 }
 
-export default ListAllProductDetails;
+export default ListAllProductDetails
